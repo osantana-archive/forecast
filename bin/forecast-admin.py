28c06a0cc57190b423cd75346547d4d14f70604e
Osvaldo Santana Neto <osantana@gmail.com>
2013-02-06 13:59:06 -0200

    New forecast-admin.py

--- bin/forecast-admin.py
+++ bin/forecast-admin.py
@@ -1,1 +1,25 @@
-../forecast/skels/project/manage.py
+#!/usr/bin/env python
+# coding: utf-8
+# pragma: no cover
+
+
+import sys
+
+from forecast.exceptions import ForecastError
+from forecast.environment import Environment
+from forecast.project import Project
+
+
+def main():
+    environment = Environment(base_list=[])
+    project = Project(environment=environment)
+    try:
+        project.run()
+    except ForecastError, ex:
+        import traceback
+        traceback.print_exc()
+        sys.exit(ex.errno)
+
+
+if __name__ == '__main__':
+    main()
